35e533c2cb3f49ff2fc5d2128150af5d45b04efb
ben <ben@techoffice.com>
2018-06-16 02:15:33 +0800

    update

---
+++ 010-Core/040-UI/020-WxPython/010-HelloWorld.py
@@ -0,0 +1,25 @@
+import wx
+import threading
+import atexit
+
+def disable_asserts():
+    import wx
+    wx.DisableAsserts()
+
+atexit.register(disable_asserts)
+
+class myThread (threading.Thread):
+	def __init__(self):
+		threading.Thread.__init__(self)
+	
+	def run(self):
+		import wx 
+		app = wx.App(False) 
+		frame = wx.Frame(None,wx.ID_ANY,"Hello") 
+		frame.Show(True) 
+		app.MainLoop()
+		
+		
+t = myThread();
+t.start();
+t.join();
