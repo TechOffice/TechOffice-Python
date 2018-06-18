203b158d5414bdb2d6e49b0b846913dd4d1835ff
ben <ben@techoffice.com>
2018-06-18 23:12:30 +0800

    Update

--- 010-Core/030-Internet/010-SimpleHttpServer/020-BaseHTTPRequestHandler.py
+++ 010-Core/030-Internet/010-SimpleHttpServer/020-BaseHTTPRequestHandler.py
@@ -6,7 +6,13 @@ class Handler(http.server.BaseHTTPRequestHandler):
 		self.send_response(200)
 		self.send_header("content-type", "text/html");
 		self.end_headers();
-		self.wfile.write(bytes("<html><body><h1>Testing</h1></body></html>", "utf-8"));
+		content = """
+			<html>
+				<body>
+					<h1>Testing</h1>
+				</body>
+			</html>""";
+		self.wfile.write(bytes(content, "utf-8"));
 		self.wfile.flush()
 		
 	def do_POST(self):
@@ -14,7 +20,13 @@ class Handler(http.server.BaseHTTPRequestHandler):
 		self.send_response(200)
 		self.send_header("content-type", "text/html");
 		self.end_headers();
-		self.wfile.write(btyes("<html><body><h1>Testing</h1></body></html>","utf-8"));
+		content = """
+			<html>
+				<body>
+					<h1>Testing</h1>
+				</body>
+			</html>""";
+		self.wfile.write(bytes(content, "utf-8"));
 		self.wfile.flush()
 		
 httpd = http.server.HTTPServer(("", 8000), Handler);
---
+++ 010-Core/030-Internet/010-SimpleHttpServer/README.md
@@ -0,0 +1,4 @@
+
+
+# Reference
+* https://docs.python.org/3/library/http.server.html#http.server.SimpleHTTPRequestHandler.do_GET
